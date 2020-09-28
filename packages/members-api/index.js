9a91b71e164ccdf4eab4295bafd7d00efb5fac2f
Fabien O'Carroll <fabien@allou.is>
2020-09-28 16:44:45 +0100

    fixup! Fixed a bug with setting geolocation

--- packages/members-api/index.js
+++ packages/members-api/index.js
@@ -182,8 +182,8 @@ module.exports = function MembersApi({
         // max request time is 500ms so shouldn't slow requests down too much
         let geolocation = JSON.stringify(await getGeolocationFromIP(ip));
         if (geolocation) {
-            const data = Object.assign(member.toJSON(), {geolocation});
-            await users.update(data, {id: member.id});
+            member.geolocation = geolocation;
+            await users.update(member, {id: member.id});
         }
 
         return getMemberIdentityData(email);
