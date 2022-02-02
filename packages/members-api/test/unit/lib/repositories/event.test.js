f02cd2e884b59823c4dd592c3e2e0f88d80e7dd6
Fabien "egg" O'Carroll <fabien@allou.is>
2022-02-02 16:36:09 +0200

    Fixed tests

--- packages/members-api/test/unit/lib/repositories/event.test.js
+++ packages/members-api/test/unit/lib/repositories/event.test.js
@@ -4,7 +4,7 @@ const sinon = require('sinon');
 const errors = require('@tryghost/errors');
 
 describe('EventRepository', function () {
-    describe('getNQLSubset', function () {
+    describe('validateAndParseEventTimelineFilter', function () {
         let eventRepository;
 
         before(function () {
@@ -21,35 +21,35 @@ describe('EventRepository', function () {
 
         it('throws when processing a filter with parenthesis', function () {
             should.throws(() => {
-                eventRepository.getNQLSubset('(type:1)');
+                eventRepository.validateAndParseEventTimelineFilter('(type:1)');
             }, errors.IncorrectUsageError);
             should.throws(() => {
-                eventRepository.getNQLSubset('type:1+(data.created_at:1+data.member_id:1)');
+                eventRepository.validateAndParseEventTimelineFilter('type:1+(data.created_at:1+data.member_id:1)');
             }, errors.IncorrectUsageError);
         });
 
         it('throws when using properties that aren\'t in the allowlist', function () {
             should.throws(() => {
-                eventRepository.getNQLSubset('(types:1)');
+                eventRepository.validateAndParseEventTimelineFilter('(types:1)');
             }, errors.IncorrectUsageError);
         });
 
         it('throws when using an OR', function () {
             should.throws(() => {
-                eventRepository.getNQLSubset('type:1,data.created_at:1');
+                eventRepository.validateAndParseEventTimelineFilter('type:1,data.created_at:1');
             }, errors.IncorrectUsageError);
 
             should.throws(() => {
-                eventRepository.getNQLSubset('type:1+data.created_at:1,data.member_id:1');
+                eventRepository.validateAndParseEventTimelineFilter('type:1+data.created_at:1,data.member_id:1');
             }, errors.IncorrectUsageError);
 
             should.throws(() => {
-                eventRepository.getNQLSubset('type:1,data.created_at:1+data.member_id:1');
+                eventRepository.validateAndParseEventTimelineFilter('type:1,data.created_at:1+data.member_id:1');
             }, errors.IncorrectUsageError);
         });
 
         it('passes when using it correctly with one filter', function () {
-            const res = eventRepository.getNQLSubset('type:email_delivered_event');
+            const res = eventRepository.validateAndParseEventTimelineFilter('type:email_delivered_event');
             res.should.be.an.Object();
             res.should.deepEqual({
                 type: 'type:email_delivered_event'
@@ -57,7 +57,7 @@ describe('EventRepository', function () {
         });
 
         it('passes when using it correctly with multiple filters', function () {
-            const res = eventRepository.getNQLSubset('type:-[email_delivered_event,email_opened_event,email_failed_event]+data.created_at:<0+data.member_id:123');
+            const res = eventRepository.validateAndParseEventTimelineFilter('type:-[email_delivered_event,email_opened_event,email_failed_event]+data.created_at:<0+data.member_id:123');
             res.should.be.an.Object();
             res.should.deepEqual({
                 'data.created_at': 'data.created_at:<0',
@@ -67,7 +67,7 @@ describe('EventRepository', function () {
         });
 
         it('passes when using it correctly with multiple filters used several times', function () {
-            const res = eventRepository.getNQLSubset('type:-email_delivered_event+data.created_at:<0+data.member_id:123+type:-[email_opened_event,email_failed_event]+data.created_at:>10');
+            const res = eventRepository.validateAndParseEventTimelineFilter('type:-email_delivered_event+data.created_at:<0+data.member_id:123+type:-[email_opened_event,email_failed_event]+data.created_at:>10');
             res.should.be.an.Object();
             res.should.deepEqual({
                 'data.created_at': 'data.created_at:<0+data.created_at:>10',
@@ -100,37 +100,12 @@ describe('EventRepository', function () {
             fake.resetHistory();
         });
 
-        it('works when setting no filters', async function () {
+        it('Passes the filters to the model layer', async function () {
             await eventRepository.getNewsletterSubscriptionEvents({
-                filter: 'no used'
-            }, {
-                type: 'unused'
+                filter: 'member_id:123'
             });
-            fake.calledOnceWithExactly({
-                withRelated: ['member'],
-                filter: ''
-            }).should.be.eql(true);
-        });
-
-        it('works when setting a created_at filter', async function () {
-            await eventRepository.getNewsletterSubscriptionEvents({}, {
-                'data.created_at': 'data.created_at:123'
-            });
-            fake.calledOnceWithExactly({
-                withRelated: ['member'],
-                filter: 'created_at:123'
-            }).should.be.eql(true);
-        });
-
-        it('works when setting a combination of filters', async function () {
-            await eventRepository.getNewsletterSubscriptionEvents({}, {
-                'data.created_at': 'data.created_at:123+data.created_at:<99999',
-                'data.member_id': 'data.member_id:-[3,4,5]+data.member_id:-[1,2,3]'
-            });
-            fake.calledOnceWithExactly({
-                withRelated: ['member'],
-                filter: 'created_at:123+created_at:<99999+member_id:-[3,4,5]+member_id:-[1,2,3]'
-            }).should.be.eql(true);
+            fake.args[0][0].withRelated.should.deepEqual(['member']);
+            fake.args[0][0].filter.should.equal('member_id:123');
         });
     });
 
@@ -159,43 +134,11 @@ describe('EventRepository', function () {
 
         it('works when setting no filters', async function () {
             await eventRepository.getEmailFailedEvents({
-                filter: 'no used',
                 order: 'created_at desc'
-            }, {
-                type: 'unused'
             });
-            fake.calledOnceWithExactly({
-                withRelated: ['member', 'email'],
-                filter: 'failed_at:-null',
-                order: 'failed_at desc'
-            }).should.be.eql(true);
-        });
-
-        it('works when setting a created_at filter', async function () {
-            await eventRepository.getEmailDeliveredEvents({
-                order: 'created_at desc'
-            }, {
-                'data.created_at': 'data.created_at:123'
-            });
-            fake.calledOnceWithExactly({
-                withRelated: ['member', 'email'],
-                filter: 'delivered_at:-null+delivered_at:123',
-                order: 'delivered_at desc'
-            }).should.be.eql(true);
-        });
-
-        it('works when setting a combination of filters', async function () {
-            await eventRepository.getEmailOpenedEvents({
-                order: 'created_at desc'
-            }, {
-                'data.created_at': 'data.created_at:123+data.created_at:<99999',
-                'data.member_id': 'data.member_id:-[3,4,5]+data.member_id:-[1,2,3]'
-            });
-            fake.calledOnceWithExactly({
-                withRelated: ['member', 'email'],
-                filter: 'opened_at:-null+opened_at:123+opened_at:<99999+member_id:-[3,4,5]+member_id:-[1,2,3]',
-                order: 'opened_at desc'
-            }).should.be.eql(true);
+            fake.args[0][0].withRelated.should.deepEqual(['member', 'email']);
+            fake.args[0][0].filter.should.equal('failed_at:-null');
+            fake.args[0][0].order.should.equal('failed_at desc');
         });
     });
 });
