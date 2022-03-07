0cba168aff1d2206246a7dcbfd6c2fb3a9a8f073
Fabien "egg" O'Carroll <fabien@allou.is>
2022-03-07 14:07:34 +0000

    fixup! Removed default for visibility

--- packages/members-api/lib/repositories/product.js
+++ packages/members-api/lib/repositories/product.js
@@ -134,7 +134,7 @@ class ProductRepository {
      * @param {object} data
      * @param {string} data.name
      * @param {string} data.description
-     * @param {boolean} data.visibility
+     * @param {'public'|'none'} data.visibility
      * @param {string} data.welcome_page_url
      * @param {BenefitInput[]} data.benefits
      * @param {StripePriceInput[]} data.stripe_prices
@@ -288,7 +288,7 @@ class ProductRepository {
      * @param {string} data.id
      * @param {string} data.name
      * @param {string} data.description
-     * @param {boolean} data.visibility
+     * @param {'public'|'none'} data.visibility
      * @param {string} data.welcome_page_url
      * @param {BenefitInput[]} data.benefits
      *
