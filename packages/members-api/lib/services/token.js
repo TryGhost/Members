6b6d81b14e830a53b35048a88583b67c35ab7003
Elijah <3681218+elijahsgh@users.noreply.github.com>
2022-05-20 11:53:25 -0500

    Fix kid missing from JWT header
    The JWT should include a header field for _kid_ per the RFC. This fixes compatibility with libraries like pyjwt that rely on being able to fetch the public keys (jwks) and match them to the token (jws, jwt).

--- packages/members-api/lib/services/token.js
+++ packages/members-api/lib/services/token.js
@@ -18,8 +18,8 @@ module.exports = class TokenService {
         const jwk = await this._keyStoreReady;
         return jwt.sign({
             sub,
-            kid: jwk.kid
         }, this._privateKey, {
+            keyid: jwk.kid,
             algorithm: 'RS512',
             audience: this._issuer,
             expiresIn: '10m',
